7433625abdc3347b9b5f7a6914ff20fe22c437e4
OldsSchoolEmpresa <posadaalberto48@gmail.com>
2025-06-23 17:50:15 -0500

    Forzar reanálisis SonarCloud con cambios visibles

--- almacen/src/app/componentes/jordan-retro1/jordan-retro1.component.ts
+++ almacen/src/app/componentes/jordan-retro1/jordan-retro1.component.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-jordan-retro1',
-  standalone: true,
-  imports: [],
   templateUrl: './jordan-retro1.component.html',
-  styleUrl: './jordan-retro1.component.css'
+  standalone: true,
+  imports: [ThumbnailsComponent]
 })
 export class JordanRetro1Component {
+  imagenesJordan = [
+    'asset/img/Jordan retro 1 negro.jpeg',
+    'asset/img/Jordan retro 1 azul.jpeg',
+    'asset/img/Jordan retro 1 verde.jpeg'
+  ];
 
+  imagenSeleccionada = this.imagenesJordan[0];
+
+  cambiarImagen(imagen: string): void {
+    this.imagenSeleccionada = imagen;
+  }
 }
